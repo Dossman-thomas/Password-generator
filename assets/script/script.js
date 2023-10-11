3f32ac5c9cbcfa0b8d6fe6683b16608cccf16348
Tom Dossman <tdossman.development@gmail.com>
2023-10-10 22:19:50 -0400

    user criteria checked off

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,90 +1,120 @@
 
-// prompt user for parameters and save in variables
-
-var length = prompt("enter password length at least 8 characters long");
-
-var upperCase = confirm("Does your password require uppercase letters?");
-
-var lowerCase = confirm("Does your password require lowercase letters?");
-  
-var numbers = confirm("Does your password require numbers?");
-
-var symbols = confirm("Does your password require special characters?");
-
-// console.log(length);
-
-// saves button w/ID=generate into variable
+// saves button w/ID=generate into variable:
 
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button: 
 
-// var length, upperCase, lowerCase, numbers, symbols
+generateBtn.addEventListener("click", writePassword);
 
 // generate password function:
 
 function generatePassword(){
 
-// initialize charset variable
+// enforce length min and max parameters
 
-  let charSet = '';
+// initialize max and mins: 
 
-// if statements returning boolean value, which either adds a new charSet or not. 
+var pwMin = 8;
+
+var pwMax = 128;
+
+var pwLength = parseInt(prompt("Enter password length between 8 to 128 characters long."));
+
+if (pwLength < pwMin || pwLength > pwMax) {
+
+  alert("Password length must be between 8 and 128 characters long.");
+
+  console.log(pwLength);
+
+  return generatePassword();
+}
+
+if (isNaN(pwLength)){
+return;
+}
+
+
+
+// prompt user for parameters and save in variables:
+
+  var upperCase = confirm("Does your password require uppercase letters?");
+
+  var lowerCase = confirm("Does your password require lowercase letters?");
+    
+  var numbers = confirm("Does your password require numbers?");
+
+  var symbols = confirm("Does your password require special characters?");
+
+
+// initialize charset variable:
+
+  var charSet = '';
+
+// if statements returning boolean value, which either adds a new charSet or not.
 
   if (upperCase){
-    charSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    charSet += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   } 
 
   if (lowerCase){
-    charSet += 'abcdefghijklmnopqrstuvwxyz'
+    charSet += 'abcdefghijklmnopqrstuvwxyz';
   } 
 
   if (numbers){
-    charSet += '0123456789'
+    charSet += '0123456789';
   } 
 
   if (symbols){
-    charSet += '!@#$%^&*?'
-  } 
+    charSet += '!@#$%^&*?';
+  }
+  
 
-// initialize password array 
+  if (!upperCase && !lowerCase && !numbers && !symbols){
+    alert("Inadequate parameters set. Please try again.");
+  }
+  
+  let finalPassword = checkCharacters(charSet, pwLength);
+  // return the password: 
+
+  return finalPassword.join('');
+
+}
+
+function checkCharacters (charSet, pwLength){
+  // initialize password array 
 
   let pwArray = [];
 
 // while the array length is less than the user input length, loop will process new characters to add to the empty character set. 
 
-  while (pwArray.length < length){
+  while (pwArray.length < pwLength){
 
-    const char = charSet[Math.floor(math.random() * charset.length)];
+    const char = charSet[Math.floor(Math.random() * charSet.length)];
 
-    if (!noDups || !pwArray.includes(char)) {
-      pwArray.push(char)
-    }
+    pwArray.push(char);
 
   }
+  
+  // check if parameter is included depending on true booleans from if statements 
+  // call function checkCharacters until true
+  // return pwArray >
 
-  // return the password: 
-
-  return pwArray.join('');
-
+  return pwArray;
 }
+
 
 function writePassword() {
 
-// reference all input variables
-
-// generate a random password 
-
-var password = generatePassword();
-
+  var password = generatePassword();
 
   var passwordText = document.querySelector("#password");
-
-// render password on page.
+    
+  // render password on page.
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
 
